c5d15b4003ecbe28f542a094621cedafc75a1524
fakendzon <mihal_palich.93@mail.ru>
2017-09-14 17:51:47 +0700

    css

--- js/main.js
+++ js/main.js
@@ -25,30 +25,13 @@ function styleDisplay(tag) {
 
 function spoiler(obj) {
     var preText = obj.parentNode.getElementsByTagName("pre"); 
-    console.log(preText);
     if (preText.length > 0) {
-        styleDisplay(preText);
+        styleDisplay(preText[0]);
     } else {
-    var newPre = document.createElement('pre');
-    // newPre.className = "text";
-    newPre.innerHTML = 'HI';
-    pre3.appendChild(newPre);
+        var newPre = document.createElement('pre');
+        newPre.setAttribute("id", "text");
+        obj.parentNode.appendChild(newPre);
+        $('#text').load("music/" + obj.text.replace(new RegExp(" ",'g'),"%20") + ".txt");
+        newPre.removeAttribute('id');
     }
-
-    // obj.parentNode.childNode.getElementsByTagName("audio");
-    // var arr = Array.prototype.slice.call(obj.parentNode.children)
-    // console.log(typeof(ar));
-    // console.log(obj.parentNode.children.namedItem('pre'));
-    // if (obj.parentNode.childNodes.indexOf('audio')) {console.log('y');}
-    // $('.text').load("../music/Holly%20Henry%20–%20Seven%20Nation%20Army%20(teemid%20remix).txt")
-    obj.parentNode.id = "pre";
-    if (obj.parentNode.getElementsByTagName("pre").length > 0) {console.log('true');}
-    document.getElementById('pre').removeAttribute('id');
 }
-
-// function spoiler(id) {
-//     if (document.getElementById(id).style.display=='none') {
-//         document.getElementById(id).style.display=''}
-//     else {
-//         document.getElementById(id).style.display='none'
-// }}
